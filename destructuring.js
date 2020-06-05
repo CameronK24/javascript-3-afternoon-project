58e20484e8c6f8598d7bfc7d38a39ebaf790801e
CameronK24 <cskelly15@gmail.com>
2020-06-05 15:24:48 -0600

    finished first project

--- destructuring.js
+++ destructuring.js
@@ -23,7 +23,7 @@ var carDetails = {
 
 //Code Here
 
-
+let {color, make, model, year} = carDetails;
 
 ////////// PROBLEM 2 //////////
 
@@ -35,6 +35,8 @@ var carDetails = {
 
 function greeting( obj ) {
   //Code Here
+
+  let {title, firstName, lastName} = obj;
   
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
@@ -55,7 +57,10 @@ function greeting( obj ) {
 
 //Code Here
 
-
+function totalPopulation(obj) {
+  let {utah, california, texas, arizona} = obj;
+  return utah + california + texas + arizona;
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -69,7 +74,11 @@ function greeting( obj ) {
 
 //Code Here
 
-
+function ingredients(obj) {
+  let {carb, fat, protein} = obj;
+  let array = [carb, fat, protein];
+  return array;
+}
 
 ////////// PROBLEM 5 //////////
 
@@ -87,7 +96,17 @@ function greeting( obj ) {
 
 //Code Here
 
-
+function largeNumbers ({first, second, third}) {
+  if(first < second && first < third) {
+    return first;
+    }
+  else if(second < first && second < third) {
+    return second;
+  }
+  else {
+    return third;
+  }  
+}
 
 ////////// PROBLEM 6 //////////
 
@@ -99,4 +118,14 @@ function greeting( obj ) {
 
 //Code Here
 
-
+function numberGroups({a, b, c}) {
+  if(a.length > b.length && a.length > c.length) {
+    return a;
+  }
+  else if(b.length > a.length && b.length > c.length) {
+    return b;
+  }
+  else {
+    return c;
+  }
+}
